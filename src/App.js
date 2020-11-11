cfee1a584edc4044cacb13ac010502d9ec073468
JRC <jrcelebrados@gmail.com>
2020-11-11 18:55:17 -0500

    final commit, I will become a React pro

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Batman423cover from './Batman423cover.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Batman423cover} className="App-logo" alt="logo" />
         <p>
           My name is Jesus Celebrados.
         </p>
         <a
+          // className="App-link"
+          // href="https://reactjs.org"
+          // target="_blank"
+          // rel="noopener noreferrer"
+
           className="App-link"
-          href="https://reactjs.org"
+          href="https:/github.com/JRCelebrados"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my website.
         </a>
       </header>
     </div>
